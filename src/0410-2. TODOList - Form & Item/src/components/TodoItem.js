0d8374424e078ed8a563e4282745056e38c9df2e
Minchul Kang <tegongkang@gmail.com>
2020-04-10 15:50:14 +0900

    lecture code

--- src/0410-2. TODOList - Form & Item/src/components/TodoItem.js
+++ src/0410-2. TODOList - Form & Item/src/components/TodoItem.js
@@ -6,17 +6,27 @@ class TodoItem extends Component {
     const { text, checked, id, onToggle, onRemove } = this.props;
 
     return (
-      <div className="todo-item" onClick={() => onToggle(id)}>
-        <div className="remove" onClick={(e) => {
-          e.stopPropagation(); // onToggle 이 실행되지 않도록 함
-          onRemove(id)}
+      // 클릭하면 onToggle 함수 실행 : 했다 안했다 체크용
+      // 체크 값이 활성화(checked)되어있으면 우측에 체크마크 (✓ &#x2713;) 를 보여주고,
+      // 마우스가 위에 있을때는 좌측에 엑스마크 (× &times;) 보여주기
+      
+      // onClick={() => onToggle(id)}  VS onClick={onToggle{id}}
+      <div className="todo-item" onClick={() => onToggle(id)}> 
+        
+        <div className="remove" onClick={(e) => { // 아이템을 삭제시키는 함수
+          e.stopPropagation();                    // onToggle 이 실행되지 않도록 함
+          onRemove(id)
+          }
         }>&times;</div>
-        <div className={`todo-text ${checked && 'checked'}`}>
+        
+        <div className={`todo-text ${checked && 'checked'}`}> 
           <div>{text}</div>
         </div>
+        
         {
-          checked && (<div className="check-mark">✓</div>)
+          checked && (<div className="check-mark">✓</div>)   // check 되어있으면 checkmark 보이기
         }
+
       </div>
     );
   }
